5e8cd44e7499ec0ee22e4fc5715e00885d7c41a3
Rilen Agustin <rilennicoleagustin8@gmail.com>
2026-08-02 19:38:38 +0800

    add buttons

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 let score = 0;
+
+const display = document.querySelector("#val");
+const msg = document.querySelector("#msg");
+
+const addBtn = document.querySelector("#add_btn");
+const subtractBtn = document.querySelector("#subtract_btn");
+const resetBtn = document.querySelector("#reset_btn");
+
+function checkConditions() {
+    if (score >= 10) {
+        msg.innerText = "Message: Outstanding performance!";
+    }
+
+    else if (score < 0) {
+        msg.innerText = "Message: Below expectations!";
+    }
+
+    else {
+        msg.innerText = 'Message: "Keep going to raise your score!"';
+    }
+}
+addBtn.addEventListener("click", () => {
+    score++;
+    display.innerText = "Current Score: " + score;
+    checkConditions();
+});
+subtractBtn.addEventListener("click", () => {
+    score--;
+    display.innerText = "Current Score: " + score;
+    checkConditions();
+});
+resetBtn.addEventListener("click", () => {
+    score = 0;
+    display.innerText = "Current Score: " + score;
+    msg.innerText = 'Message: "Keep going to raise your score!"';
+});
